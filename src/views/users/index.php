52c272a83e13c95b1958a94c25ab3efb9b115d8b
Floriian <lovasifloriian@outlook.hu>
2023-06-01 00:30:45 +0200

    display users

--- src/views/users/index.php
+++ src/views/users/index.php
@@ -1,5 +1,13 @@
 <?php
-foreach($users as $user) {
-    echo $user['name'];
-}
+require_once("src/views/header.php");
+?>
+
+<div class="box">
+    <?php foreach($users as $user): ?>
+        <p class="text"><?php echo $user['name'] ?></p>
+    <?php endforeach; ?>
+</div>
+
+<?php
+require_once("src/views/footer.php");
 ?>
