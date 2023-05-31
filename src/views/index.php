c710bf3d3d6340b26d664430bced47df37a1dd15
Floriian <lovasifloriian@outlook.hu>
2023-06-01 01:40:41 +0200

    create index page & indexpage minimalistic styling

--- src/views/index.php
+++ src/views/index.php
@@ -1,3 +1,16 @@
 <?php
-echo "Index PHP"
+require_once("src/views/header.php");
+?>
+<div class="pageSelector">
+    <div class="box">
+        <a href="/users">Users</a>
+    </div>
+    <div class="box">
+        <a href="/advertisements">
+            Advertisements
+        </a>
+    </div>
+</div>
+<?php
+require_once("src/views/footer.php");
 ?>
