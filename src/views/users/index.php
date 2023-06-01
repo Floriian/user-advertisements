2561ca131c3f8df402ae14ec8e89e9fe41d1e5e6
Floriian <lovasifloriian@outlook.hu>
2023-06-01 13:56:00 +0200

    add title add dataContainer

--- src/views/users/index.php
+++ src/views/users/index.php
@@ -1,11 +1,13 @@
 <?php
 require_once("src/views/header.php");
 ?>
-
-<div class="box">
-    <?php foreach($users as $user): ?>
-        <p class="text"><?php echo $user['name'] ?></p>
-    <?php endforeach; ?>
+<h1 class="title">User List</h1>
+<div class="dataContainer">
+    <div class="box">
+        <?php foreach($users as $user): ?>
+            <p class="text"><?php echo $user['name'] ?></p>
+        <?php endforeach; ?>
+    </div>
 </div>
 
 <?php
