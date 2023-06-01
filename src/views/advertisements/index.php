d3e75f1cd3d041676615c7be7237a59c4d8daa33
Floriian <lovasifloriian@outlook.hu>
2023-06-01 13:55:48 +0200

    add text class to advertisements paragraph add dataContainer format code for better reading

--- src/views/advertisements/index.php
+++ src/views/advertisements/index.php
@@ -1,14 +1,20 @@
 <?php
 require_once("src/views/header.php");
-?>
 
-<div class="box">
-    <?php foreach($advertisements as $advertisement): ?>
-        <p class="text">
-            <?php echo $advertisement['title'] ?>
-            <span>(<?php echo $advertisement['name'] ?>)</span>
-        </p>
-    <?php endforeach; ?>
+?>
+<h1 class="title">Advertisements List</h1>
+<div class="dataContainer">
+    <div class="box">
+        <?php foreach($advertisements as $advertisement): ?>
+            <p>
+                <?php echo $advertisement['title'] ?>
+                <span>(
+                    <?php echo $advertisement['name'] ?>
+                )
+                </span>
+            </p>
+        <?php endforeach; ?>
+    </div>
 </div>
 
 <?php
